7774e2e0c17c2b7ca88c4bbec0bd299ea902afc4
Niharika-Khare <30350859+Niharika-Khare@users.noreply.github.com>
2019-07-17 01:40:59 +0530

    UI

--- popup.js
+++ popup.js
@@ -1,10 +1,22 @@
 
-
 document.addEventListener('DOMContentLoaded', function() {
     var popupButton = document.getElementById('popupButton');
 
     // onClick's logic below:
     popupButton.addEventListener('click', function() {
+    	//  $.ajax({
+     //  		type:'get',
+     //  		url:'/URLToTriggerGetRequestHandler',
+     //  		cache:false,
+     //  		async:'asynchronous',
+     //  		dataType:'json',
+     //  		success: function(data) {
+     //    		console.log(JSON.stringify(data))
+     //  		},
+     //  		error: function(request, status, error) {
+     //    		console.log("Error: " + error)
+     //  		}
+   		// });
     	window.open("timestamp.html", "_parent");
     });
 
--- timestamp.js
+++ timestamp.js
@@ -5,6 +5,7 @@ function processData(allText) {
 
     for (var i=0; i<allTextLines.length; i++) {
     	
+    	// Obtain all fields separately
         var data = allTextLines[i].split(' ');
 
         // Set text for start and end timestamp
@@ -19,40 +20,51 @@ function processData(allText) {
         // Append Time Stamp data with url in table row
         var td1 = document.createElement('td');
         var button1 = document.createElement('button');
+
+        // Add CSS to button
         button1.class = "timestamp_url";
-        button1.innerHTML = url_text;
+        button1.innerHTML = "<b>" + url_text + "</b>";
+        button1.style.background = "#15c39a";
   		button1.style.border = "none";
-  		button1.style.color = "#494b4f";
-  		button1.style.padding = "5px";
+  		button1.style.color = "white";
+  		button1.style.fontSize = "14px";
+  		button1.style.fontFamily = '"Segoe UI",Arial,sans-serif';
   		button1.style.display = "inline-block";
-  		button1.style.margin = "4px 2px";
   		button1.style.cursor = "pointer";
   		button1.style.width = "100%";
+  		button1.style.padding = "5px 0px 5px 0px";
+  		button1.style.borderRadius = "8px";
 
   		button1.addEventListener('click', function() {
     		chrome.tabs.getCurrent(function (tab) {
-  				//Update the url here.
+  				//Update the URL of current window here
   				chrome.tabs.update({url: newUrl});
 			});
     	});
 
         td1.appendChild(button1);
-        td1.style.width = "200px";
+        td1.style.width = "150px";
+        td1.style.padding = "10px";
+        td1.style.margin = "4px 0px 4px 0px";
         tr.appendChild(td1);
 
         // Add Keyword to corresponding URL
         var td2 = document.createElement('td');
         var div1 = document.createElement('div');
+
+        // Add CSS to div
         div1.class = "text_area"
         div1.innerHTML = data[2];
-        div1.style.padding = "5px";
-        div1.style.margin = "4px 2px";
+        div1.style.padding = "8px 3px 8px 3px";
         td2.appendChild(div1);
+        td2.style.width = "250px";
+        td2.style.padding = "10px";
+        td2.style.margin = "4px 0px 4px 0px";
         tr.appendChild(td2);
 
         table.appendChild(tr);
     }
-    table.style.width = "500px";
+    table.style.width = "450px";
     document.body.appendChild(table);
 }
 
